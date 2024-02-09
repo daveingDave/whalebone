ff111f7d836b0507a849051bfb19add175217875
david.markovic <david.markovic@artin.cz>
2024-02-09 08:22:19 +0100

    Increase timeout for one test, because of failing pipeline

--- tests/api.spec.js
+++ tests/api.spec.js
@@ -28,6 +28,7 @@ test('Count all teams', async () => {
 });
 
 test('Oldest Team & Player Count Test', async ({page}) => {
+  test.setTimeout(10000)
   const data = await fetchData();
   if (!data) return;
 
